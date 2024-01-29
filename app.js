63345e416d488fcba4e1d15417a6c0a5d5373c40
pavantechie1862 <pavanmarapalli171862@gmail.com>
2024-01-29 17:37:31 +0530

    origin changed

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ require("dotenv").config();
 const port = process.env.PORT;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://main.d2j3zrqyjtpuhw.amplifyapp.com/"
+  );
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");

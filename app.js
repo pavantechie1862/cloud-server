41ed85e47e6408468bd468a937d130c7871aaf1e
pavantechie1862 <pavanmarapalli171862@gmail.com>
2024-01-30 11:27:37 +0530

    removed origin

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   console.log("hello world in cors");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://main.d2j3zrqyjtpuhw.amplifyapp.com"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");

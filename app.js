6e2eebb65829972cd3b52e268d863dbef2240e36
pavantechie1862 <pavanmarapalli171862@gmail.com>
2024-01-30 11:30:21 +0530

    removed object

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ require("dotenv").config();
 const port = process.env.PORT;
 app.use(cors());
 
-app.use((req, res, next) => {
-  console.log("hello world in cors");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("hello world in cors");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 // app.use(cors());
 
